a3c5243d4834591d60547e9dc1799d235cb9e4c1
Krish-megabytes <divyanshu09811@gmail.com>
2025-06-11 16:58:56 +0530

    Update clock.js

--- clock.js
+++ clock.js
@@ -5,7 +5,7 @@ function updateclock() {
   let seconds = now.getseconds().toString().padStart(2, "0");
   let updatestring = hours + ":" + minutes + ":" + seconds;
 
-  document.getElementsByid('clockbox').innertext = updatestring;
+  document.getElementsByid('clock').innertext = updatestring;
 }
 
 updateclock();
